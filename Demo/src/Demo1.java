639dabbc5089d61c99b6c5b55a4f3e7fbbfc037e
cjj <1163317389@qq.com>
2019-04-03 15:20:14 +0800

    初始化

---
+++ Demo/src/Demo1.java
@@ -0,0 +1,20 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        String s1 = "sasakjla";
+        StringBuffer sb6 = new StringBuffer(s1);
+        System.out.println(sb6);
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("cjjcjj");
+
+        String sb1 = sb.substring(1,3);
+        String sb3 = sb.toString();
+        System.out.println(sb3);
+
+    }
+
+    public static void stringDemo(){
+
+    }
+}
